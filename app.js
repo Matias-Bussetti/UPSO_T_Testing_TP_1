a541cde76f0db8edf418b7291a0d077fd5cdc2b9
Matias-Bussetti <desarrollos.informaticos.bsstt@gmail.com>
2023-10-28 15:07:56 -0300

    delete y edit funciona

--- app.js
+++ app.js
@@ -55,6 +55,50 @@ class Student {
     );
   }
 
+  updateAttributesFromObject({
+    id,
+    name,
+    surName,
+    role,
+    email,
+    yearBorn,
+    code,
+    password,
+    canEnrol,
+  }) {
+    this.name = name;
+    this.surName = surName;
+    this.role = role;
+    this.email = email;
+    this.yearBorn = yearBorn;
+    this.code = code;
+    this.password = password;
+    this.canEnrol = canEnrol;
+
+    const collectionOfStudents =
+      LocalStorageInterface.getCollection("students");
+
+    LocalStorageInterface.storeCollection(
+      collectionOfStudents.map((student) => {
+        if (student.id == id) {
+          return this;
+        }
+        return student;
+      }),
+      "students"
+    );
+  }
+
+  delete() {
+    const collectionOfStudents =
+      LocalStorageInterface.getCollection("students");
+
+    LocalStorageInterface.storeCollection(
+      collectionOfStudents.filter((student) => student.id != this.id),
+      "students"
+    );
+  }
+
   static getStudentFromId(id) {
     const collectionOfStudents =
       LocalStorageInterface.getCollection("students");
@@ -85,6 +129,11 @@ class LocalStorageInterface {
     localStorage.setItem(storageKeyName, JSON.stringify(collectionFromStorage));
   }
 
+  static storeCollection(collection, storageKeyName) {
+    localStorage.removeItem(storageKeyName);
+    localStorage.setItem(storageKeyName, JSON.stringify(collection));
+  }
+
   static getCollection(storageKeyName) {
     return localStorage.getItem(storageKeyName)
       ? JSON.parse(localStorage.getItem(storageKeyName))
@@ -103,32 +152,97 @@ class DomManipulator {
     formStudent.onsubmit = (e) => {
       e.preventDefault();
 
-      var student = new Student(
-        0,
-        "N",
-        "B",
-        1,
-        "a@a",
-        2020,
-        "c1a2",
-        "12345678",
-        true
-      );
-      console.log(JSON.stringify(student));
+      // console.log(
+      //   formStudent.id.value,
+      //   formStudent.name.value,
+      //   formStudent.surName.value,
+      //   formStudent.canEnrol.checked,
+      //   formStudent.email.value,
+      //   formStudent.yearBorn.value,
+      //   formStudent.code.value,
+      //   formStudent.password.value
+      // );
+
+      if (formStudent.id.value == "-1") {
+        console.log(formStudent.id.value == "-1");
+
+        const newStudent = new Student(
+          Math.floor(Math.random() * 10000 + 1),
+          formStudent.name.value,
+          formStudent.surName.value,
+          formStudent.canEnrol.checked,
+          formStudent.email.value,
+          formStudent.yearBorn.value,
+          formStudent.code.value,
+          formStudent.password.value
+        );
+
+        LocalStorageInterface.storeElementInCollection(newStudent, "students");
+        this.listStudentsFromStorage();
+        formStudent.reset();
+
+        return;
+      }
+
+      const updateStudent = Student.getStudentFromId(formStudent.id.value);
+      console.log(updateStudent);
+
+      if (updateStudent) {
+        updateStudent.updateAttributesFromObject({
+          id: formStudent.id.value,
+          name: formStudent.name.value,
+          surName: formStudent.surName.value,
+          canEnrol: formStudent.canEnrol.checked,
+          email: formStudent.email.value,
+          yearBorn: formStudent.yearBorn.value,
+          code: formStudent.code.value,
+          password: formStudent.password.value,
+        });
+      }
+
+      console.log(updateStudent);
+
+      formStudent.reset();
+
+      this.listStudentsFromStorage();
+
+      //LocalStorageInterface.storeElementInCollection(student, "students", 10);
     };
   }
 
   static testStudent1() {
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function getRandomString(length) {
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      let result = "";
+      for (let i = 0; i < length; i++) {
+        const randomIndex = getRandomInt(0, characters.length - 1);
+        result += characters.charAt(randomIndex);
+      }
+      return result;
+    }
+
+    function getRandomEmail() {
+      const username = getRandomString(5);
+      const domain = getRandomString(3);
+      const tld = getRandomString(2);
+      return `${username}@${domain}.${tld}`;
+    }
+
     var student = new Student(
-      Math.floor(Math.random() * 1000),
-      "N",
-      "B",
-      1,
-      "a@a",
-      2020,
-      "c1a2",
-      "12345678",
-      true
+      getRandomInt(1, 1000), // ID
+      getRandomString(1), // Nombre
+      getRandomString(1), // Apellido
+      getRandomInt(1, 10), // Año
+      getRandomEmail(), // Correo electrónico
+      getRandomInt(2000, 2022), // Año de ingreso
+      getRandomString(4), // Código
+      getRandomInt(10000000, 99999999), // Número de teléfono
+      Math.random() < 0.5 // Estudiante activo (aleatorio 50/50)
     );
 
     LocalStorageInterface.storeElementInCollection(student, "students", 10);
@@ -166,16 +280,27 @@ class DomManipulator {
 
       listStudents.appendChild(newListElement);
 
-      var buttonEditStudent = document.createElement("button");
-
       //Buscamos el form
 
+      var buttonEditStudent = document.createElement("button");
       buttonEditStudent.onclick = () => {
         this.fillFormStudent(student);
       };
       buttonEditStudent.innerText = "edit";
       listStudents.appendChild(buttonEditStudent);
+
+      var buttonDeleteStudent = document.createElement("button");
+      buttonDeleteStudent.onclick = () => {
+        this.deleteStudent(student);
+      };
+      buttonDeleteStudent.innerText = "delete";
+      listStudents.appendChild(buttonDeleteStudent);
     });
+  }
+
+  static deleteStudent(student) {
+    Student.studentFromObject(student).delete();
+    this.listStudentsFromStorage();
   }
 
   static fillFormStudent(student) {
@@ -185,7 +310,7 @@ class DomManipulator {
         if (formStudent[attribute[0]].type == "checkbox") {
           formStudent[attribute[0]].checked = attribute[1];
         } else {
-          console.log(formStudent[attribute[0]], attribute[1]);
+          // console.log(formStudent[attribute[0]], attribute[1]);
           formStudent[attribute[0]].value = attribute[1];
         }
       }
