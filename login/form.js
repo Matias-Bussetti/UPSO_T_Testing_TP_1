3d1f74064fe34d816e1655369d928e0c0565079e
Matias-Bussetti <desarrollos.informaticos.bsstt@gmail.com>
2023-11-05 22:57:18 -0300

    cambios

--- login/form.js
+++ login/form.js
@@ -1,45 +1,50 @@
+import { Auth } from "/app.js";
+
 //para hacer verificaciones dentro del formulario
-var form = document.getElementById('form');
-    nameUs = form.name;
-    password = form.password;
-    error = document.getElementById('error');
+var form = document.getElementById("form");
+var nameUs = form.name;
+var password = form.password;
+var error = document.getElementById("error");
 
+// funcion que valida si se pusieron caracteres en el nombre
+function validateName(e) {
+  if (nameUs.value == "" || nameUs.value == null) {
+    error.style.display = "block";
+    error.innerHTML += "<li> Por favor complete el nombre del usuario</li>";
+    console.log("Por favor complete el nombre de usuario");
 
-// funcion que valida si se pusieron caracteres en el nombre 
-function validateName (e){
-    if (nameUs.value == '' || nameUs.value == null){
-        error.style.display = 'block';
-        error.innerHTML += '<li> Por favor complete el nombre del usuario</li>';
-        console.log('Por favor complete el nombre de usuario');
-
-        e.preventDefault();
-        
-    } else{
-        error.style.display= 'none';
-    }
+    e.preventDefault();
+  } else {
+    error.style.display = "none";
+  }
 }
 
 //Funcion que valida si se pusieron caracteres en la contraseña
-function validatePassword (e){
-    if (password.value == '' || password.value == null){
-        error.style.display='block';
-        error.innerHTML += '<li>Por favor complete la contraseña';
-        console.log('Por favor complete la contraseña');
+function validatePassword(e) {
+  if (password.value == "" || password.value == null) {
+    error.style.display = "block";
+    error.innerHTML += "<li>Por favor complete la contraseña";
+    console.log("Por favor complete la contraseña");
 
-        e.preventDefault();
-    }
-    else{
-        error.style.display='none';
-    }
+    e.preventDefault();
+  } else {
+    error.style.display = "none";
+  }
 }
 
-//funcion que se encarga de validar los campos 
-function  validateForm(e){
-    //reinicio el error para que inicie sin mensaje
-    error.innerHTML='';
+//funcion que se encarga de validar los campos
+function validateForm(e) {
+  e.preventDefault();
+  //reinicio el error para que inicie sin mensaje
+  error.innerHTML = "";
 
-    validateName(e);
-    validatePassword(e);
+  //validateName(e);
+  //validatePassword(e);
+
+  //Validar contraseña o username
+  if (Auth.login(nameUs.value, password.value)) {
+    alert("Mal Login!!");
+  }
 }
 
-form.addEventListener('submit',validateForm);
+form.addEventListener("submit", validateForm);
